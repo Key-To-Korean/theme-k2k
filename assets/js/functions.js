8666ca8b763d0f8c0c48f0a17596f5e3f865907c
Aaron Snowberger <jekkilekki@gmail.com>
2017-07-23 07:58:55 +0900

    Sidebar and Footer widget styling and cleanup

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,37 +6,59 @@
  * 1. Centered images full bleed
  */
 ( function( $ ) {
+    
+    /*
+     * Center Aligned Images - full bleed on smaller screens
+     */
     $( 'figure.wp-caption.aligncenter' ).removeAttr( 'style' );
     $( 'img.aligncenter' ).wrap( '<figure class="centered-image" />' );
     
-    $( '.comments-body .vcard' ).addClass( 'skewed-thumbnail' );
-    
-    // Turn on site search
+    /*
+     * Turn on site search
+     */ 
     $( '#site-search-button, .search-toggle' ).click( function() {
         if( $( '#secondary' ).hasClass( 'active' ) ) {
             $( '#secondary' ).removeClass( 'active' );
         }
         
-        $( '.site-search-overlay' ).toggleClass( 'active' );
-        $( '.site-search-overlay .search-field' ).focus();
-        
-        // Turn off site search when it's not in focus
-        $( '.site-search-overlay .search-field' ).focusout( function() {
-           $( '.site-search-overlay' ).removeClass( 'active' ); 
-        });
+        // @TODO: Needs some work to turn OFF search when button is clicked again
+        if( $( '.site-search-overlay' ).hasClass( 'active' ) ) {
+            $( '.site-search-overlay' ).removeClass( 'active' );
+        } else {
+            $( '.site-search-overlay' ).addClass( 'active' );
+            $( '.site-search-overlay .search-field' ).focus();
+        }
+ 
+    });
+    // Turn off site search when it's not in focus
+    $( '.site-search-overlay .search-field' ).focusout( function() {
+       $( '.site-search-overlay' ).removeClass( 'active' ); 
     });
     
-    //f( $( '.site-search-overlay .search-field' ) ).focus() 
-    
-    // Activate the sidebar
+    /*
+     * Activate the sidebar
+     */ 
     $( '#site-sidebar-button' ).click( function() {
         if( $( '.site-search-overlay' ).hasClass( 'active' ) ) {
             $( '.site-search-overlay' ).removeClass( 'active' );
         }
         $( '#secondary' ).toggleClass( 'active' );
     });
+    // Put back Sidebar when it's not hovered over
+    $( '#secondary' ).mouseout( function() {
+        if( $( '#secondary' ).hasClass( 'active' ) ) {
+            $( '#secondary' ).removeClass( 'active' );
+        }
+    });
     
-    // View Comments
+    /*
+     * Create skewed avatars in Comments section
+     */
+    $( '.comments-body .vcard' ).addClass( 'skewed-thumbnail' );
+    
+    /*
+     * Show / Hide Comments
+     */ 
     $( '.view-comments' ).click( function() {
        $( '.view-the-comments' ).toggleClass( 'active' ); 
     });
--- footer.php
+++ footer.php
@@ -13,11 +13,23 @@
 
 	</div>
         
-        <?php get_sidebar( 'footer' ); ?>
+        <footer id="colophon" role="contentinfo" class="site-footer <?php 
+                if ( get_header_image() ) : ?>
+                    footer-image" style="background: url('<?php header_image(); ?>') no-repeat center center fixed
+                <?php endif; ?>">
+                
+                <div class="gradient-overlay"></div>
         
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php get_template_part( 'components/footer/site', 'info' ); ?>
-	</footer>
+                <div class="footer-widgets-area">
+                    <?php get_sidebar( 'footer' ); ?>
+                </div>
+                
+                <div class="site-info-area">
+                    <?php get_template_part( 'components/footer/site', 'info' ); ?>
+                </div>
+                    
+        </footer>
+        
 </div>
 <?php wp_footer(); ?>
 
